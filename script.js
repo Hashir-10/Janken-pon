15dfad5948e60912d96f18db6fcd01ba5d806e2a
Hashir-10 <hashirzubair5742@gmail.com>
2025-07-19 13:15:01 +0500

    feat: Add necessary code to play one round

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 /* --------Step-1-------- */
 // Make a function "getComputerChoice"
 // Declare a variable named "randomValue"
-// Assign "Math.random" function to randomValue variable 
+// Assign "Math.random" function to randomValue variable
 // Multiply Math.random function by 3 to scale range from 0 to 3(exclusive)
 // Wrap Math.random function inside "Math.floor" function to round to nearest whole number
 // Write a conditon inside "getComputerChoice" function
-// Using switch-statements we will check for each case 
+// Using switch-statements we will check for each case
 // For each random value(0,1,2) assign 0 , 1 and 2 to each individual case
 // Depending on the case we will return one of the "rock, paper and scissor" values
 
@@ -32,51 +32,86 @@
 // Assign getHumanChoice function to humanChoice variable to store return value of getHumanChoice function in this variable
 // Assign getComputerChoice function to computerChoice variale to store returun value of getComputerChoice function in this variable
 // Pass variables humanChoice and computerChoice as arguments for parameters humanChoice and computerChoice respectively of playRound function
+// Log initial score into the console
 // Use conditional statement if-else to identify the winner
 // If human wins increment humanScore variable and if computer wins increment computerScore variable
 // If the match is tied nobody gets point
 // Log who won and who lost and score into the console
-// If humanChoice is equal to rock and computerChoice is equal to paper then human wins
+// If humanChoice is equal to rock and computerChoice is equal to paper then human loses
 // Else-if humanChoice is equal to paper and ComputerChoice is equal to rock then human wins
 // Else-if humanChoice is equal to scissor and computerChoice is equal paper then human wins
-// Else-if computerChoice is rock and humanChoice is paper then computer wins
-// Else-if computerChoice is paper and humanChoice is rock then computer wins
+// ELse-if humanChoice is rock and computerChoice is scissor then human wins
 // Else-if computerChoice  is scissors and humanChoice is paper then computer wins
+// Else-if computerCHoice is rock and humanChoice is scissors computer wins
 // Else the match is tied
+
 function getComputerChoice() {
-    const randomValue = Math.floor( Math.random() * 3 );
-    
-    switch (randomValue) {
-        case 0:
-            return 'rock';
-            break;
-        case 1:
-            return 'paper';
-            break;
-        case 2:
-            return 'scissor';
-            break;
-    }
+  const randomValue = Math.floor(Math.random() * 3);
+
+  switch (randomValue) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissor";
+      break;
+  }
 }
 
 function getHumanChoice() {
-    var userChoice = prompt('Heya visitor ! Wanna play a Janken-pon(rock-paper-scissors) game ? Enter one of the "rock", "paper" or "scissor" values , If you enter any other value aside from these the round will result in a tie. To see results open "dev-tools console". Good luck !'); 
-    var userChoice = userChoice.toLowerCase();
+  var userChoice = prompt(
+    'Heya visitor ! Wanna play a Janken-pon(rock-paper-scissors) game ? Enter one of the "rock", "paper" or "scissor" values , If you enter any other value aside from these the round will result in a tie. To see results open "dev-tools console". Good luck !'
+  );
+  var userChoice = userChoice.toLowerCase();
 
-    switch (userChoice) {
-        case 'rock':
-            return 'rock';
-            break;
-        case 'paper':
-            return 'paper';
-            break;
-        case 'scissor' || 'scissors':
-            return 'scissor';
-            break;
-        default:
-            return 'tie'
-    }
+  switch (userChoice) {
+    case "rock":
+      return "rock";
+      break;
+    case "paper":
+      return "paper";
+      break;
+    case "scissor":
+      return "scissor";
+      break;
+    default:
+      return "tie";
+  }
 }
-console.log( getHumanChoice() );
+
 let humanScore = 0;
 let computerScore = 0;
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
+
+function playRound(humanChoice, computerChoice) {
+    console.log('| RESULT OF THE GAME |')
+    console.log(`Starting score is = You: ${humanScore} | Computer: ${computerScore}`);
+
+    if (humanChoice == 'rock' && computerChoice == 'scissor'){
+        humanScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else if (humanChoice == 'scissor' && computerChoice == 'paper') {
+        humanScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else if (humanChoice == 'paper' && computerChoice == 'scissor') {
+        computerScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else if (humanChoice == 'scissor' && computerChoice == 'rock') {
+        computerScore++;
+        console.log(`Current score is = You: ${humanScore} | Computer: ${computerScore}`);
+    } else {
+        console.log('This round resulted in a tie | No points for anyone.');
+    }
+}
+
